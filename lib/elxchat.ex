d0f45dd4baa108bc771695494bc07ebee5288cf0
nabeelvalapra <nabeelvalapra@gmail.com>
2016-09-24 10:17:37 +0530

    uses the via_tuple to handle the pid for diff rooms

--- lib/elxchat.ex
+++ lib/elxchat.ex
@@ -3,17 +3,23 @@ defmodule ElxChat.Server do
   
   # API
 
-  def start_link(name) do 
-    GenServer.start_link(__MODULE__, [], name: :chat_room)
+  def start_link(room_name) do 
+    GenServer.start_link(__MODULE__, [], name: via_tuple(room_name))
   end
 
-  def add_message(message) do 
-    GenServer.cast(:chat_room, {:add_message, message}) 
+  def add_message(room_name, message) do 
+    GenServer.cast(via_tuple(room_name), {:add_message, message}) 
   end
 
-  def get_messages() do 
-    GenServer.call(:chat_room, :get_messages)
+  def get_messages(room_name) do 
+    GenServer.call(via_tuple(room_name), :get_messages)
   end   
+
+  # Private Functions
+  
+  defp via_tuple(room_name) do 
+    {:via, ElxChat.Registry, {:chat_room, room_name}}
+  end
 
   # Callback 
   
